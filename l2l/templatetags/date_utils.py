62b63576f49688be57353027d756eb68bc69ae78
Jordan Cooper <jordan@cooperplanet.com>
2024-07-08 08:20:33 -0600

    Support datetime objects in format_date filter

--- l2l/templatetags/date_utils.py
+++ l2l/templatetags/date_utils.py
@@ -1,8 +1,12 @@
 from django import template
+from datetime import datetime
 
 register = template.Library()
 
 
 @register.filter
-def format_date(date_string):
-    return date_string.replace("T", " ")
+def format_date(date):
+    if isinstance(date, str):
+        date_object = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        return date_object.strftime("%Y-%m-%d %H:%M:%S")
+    return date.strftime("%Y-%m-%d %H:%M:%S")
--- l2l/tests/test_date_utils.py
+++ l2l/tests/test_date_utils.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from l2l.templatetags.date_utils import format_date
+from datetime import datetime
 
 
 class TestFormatDate(TestCase):
@@ -7,4 +8,10 @@ class TestFormatDate(TestCase):
         string_date = "2024-07-01T14:30:00"
         expected = "2024-07-01 14:30:00"
         actual = format_date(string_date)
+        self.assertEqual(expected, actual)
+
+    def test_date_as_datetime(self):
+        date = datetime(2024, 7, 1, 14, 30)
+        expected = "2024-07-01 14:30:00"
+        actual = format_date(date)
         self.assertEqual(expected, actual)
